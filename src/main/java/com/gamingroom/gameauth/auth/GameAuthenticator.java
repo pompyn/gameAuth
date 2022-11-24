7ab6a5a3d26ac1435079e10514fe5a08180e7995
https://github.com/pompyn <pompyn@live.com>
2022-11-23 21:56:34 -0600

    initial commit

---
+++ src/main/java/com/gamingroom/gameauth/auth/GameAuthenticator.java
@@ -0,0 +1,30 @@
+package com.gamingroom.gameauth.auth;
+
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicCredentials;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class GameAuthenticator implements Authenticator<BasicCredentials, GameUser> {
+
+    private static final Map<String, Set<String>> VALID_USERS = ImmutableMap.of(
+            "guest", ImmutableSet.of(),
+            "user", ImmutableSet.of("USER"),
+            "admin", ImmutableSet.of("ADMIN", "USER")
+    );
+
+    @Override
+    public Optional<GameUser> authenticate(BasicCredentials credentials) throws AuthenticationException {
+        if (VALID_USERS.containsKey(credentials.getUsername()) && "password".equals(credentials.getPassword())) {
+            // FIXME: Finish the authorize method based on BasicAuth Security Example for new GameUser
+
+        }
+        return Optional.empty();
+    }
+}
